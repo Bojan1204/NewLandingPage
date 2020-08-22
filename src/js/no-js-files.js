9693cf8b65cf111833c45d7a6c13340719ff588e
Bojan <bojan.bosnic97@gmail.com>
2020-08-23 00:17:41 +0200

    commit

--- src/js/no-js-files.js
+++ src/js/no-js-files.js
@@ -3,8 +3,8 @@
 // import '../img/webpack4.jpg';
 // import '../img/bg.png';
 import '../scss/index.scss';
-//import '../img/icons/favicon.ico';
-//import '../img/icons/icon.png';
+//import '../img/favicon.ico';
+ //import '../img/icons/icon.png';
 import '../../.gitkeep';
 import '../.htaccess';
 import '../fonts/Anton-Regular.ttf';
