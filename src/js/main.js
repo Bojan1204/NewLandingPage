eb3f6a0e77d45e6212f369e75b7ac2771b5d557e
Bojan <bojan.bosnic97@gmail.com>
2021-02-09 14:54:43 +0100

    worked on  header

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,22 @@
-let normal = document.querySelector('.normalText');
- 
+let normal = document.querySelector(".normalText");
 
-const hideText =()=>{
+const hideText = () => {
     let neznam = normal.getBoundingClientRect().top;
-    let ekran = window.innerHeight / 1.4;  
-   if(neznam < ekran){
-       normal.classList.add('konj');
-   }
-   
-}
-
-addEventListener('scroll', hideText);
-
-
-const navigacija = document.querySelector('.navigacija');
-
-
-
-window.onscroll =()=>{
-    if(window.scrollY > 5){
-        navigacija.classList.add('addNavigation');
-    }else{
-        navigacija.classList.remove('addNavigation');
+    let ekran = window.innerHeight / 1.4;
+    if (neznam < ekran) {
+        normal.classList.add("showText");
     }
 };
 
+addEventListener("scroll", hideText);
 
+const navigacija = document.querySelector(".navigacija");
 
+window.onscroll = () => {
+    if (window.scrollY > 5) {
+        navigacija.classList.add("addNavigation");
+    } else {
+        navigacija.classList.remove("addNavigation");
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(window.scrollY > 1){
-//     navigacija.classList.toggle('addNavigation');
-// }else(window.scrollY > 800){
-//     navigacija.classList.toggle('addNavigationSecond');
-//     navigacija.classList.remove('addNavigation');
-
-// }
